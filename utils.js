26dbfe9b11a89610c059f7246fa37a7c60114cba
Tlknight997 <tlknight997@gmail.com>
2025-04-23 13:22:23 +0200

    Funzioni giocatore

--- utils.js
+++ utils.js
@@ -1,2 +1,10 @@
 export const giocatori = [];
 const data = new Date();
+function CreaGiocatore(nome,cognome,data_nascita,nazionalita)
+{
+    e = Math.floor(((new Date(data.toLocaleDateString('it-IT').split('/').reverse().join('-'))-new Date(data_nascita.split('/').reverse().join('-')))/1000/60/60/24)/365);
+    return{Nome:nome,Cognome:cognome,Data_nascita:data_nascita,Nazionalita:nazionalita,eta:e,gare:[]};
+}
+export function NuovoGiocatore(nome,cognome,data_nascita,nazionalita){
+    giocatori.push(CreaGiocatore(nome,cognome,data_nascita,nazionalita));
+}
