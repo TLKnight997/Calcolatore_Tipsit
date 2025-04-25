5f5a6587c8128b50d0dc44a6ba97da063e898be4
Tlknight997 <tlknight997@gmail.com>
2025-04-25 15:17:31 +0200

    Funzioni per il calcolo dei punti e delle posizioni

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 export const giocatori = [];
 export let classifica = [];
 export const gare = [];
-const data = new Date();
-function CreaGiocatore(nome,cognome,data_nascita,nazionalita)
+export const data = new Date();
+export function CreaGiocatore(nome,cognome,data_nascita,nazionalita)
 {
-    e = Math.floor(((new Date(data.toLocaleDateString('it-IT').split('/').reverse().join('-'))-new Date(data_nascita.split('/').reverse().join('-')))/1000/60/60/24)/365);
-    return{Nome:nome,Cognome:cognome,Data_nascita:data_nascita,Nazionalita:nazionalita,eta:e,gare:[]};
+    let e = Math.floor(((new Date(data.toLocaleDateString('it-IT').split('/').reverse().join('-'))-new Date(data_nascita.split('/').reverse().join('-')))/1000/60/60/24)/365);
+    return{Nome:nome,Cognome:cognome,Data_nascita:data_nascita,Nazionalita:nazionalita,eta:e,gare:[],media_punteggi:0,percentuale_vit:0,percentuale_pod:0,percentuale_fuoripod:0};
 }
 export function NuovoGiocatore(nome,cognome,data_nascita,nazionalita){
     giocatori.push(CreaGiocatore(nome,cognome,data_nascita,nazionalita));
@@ -18,3 +18,31 @@ export function NuovaGara(a) {
     gare.push({nome:a.nome,classifica:a.classifica});
         a.classifica.forEach(gara => {giocatori.forEach(giocatore => {if (giocatore.Nome === gara.nome) {giocatore.gare.push(gara);}});});
 }
+export function AggiornaPunteggi(giocatore){
+    let vit = 0,gioc = 0,percent_pod=0,percent_fuoripod=0;
+    giocatore.gare.forEach((x)=>{
+        gioc += 1;
+        if(x.pos<=3){
+            percent_pod += 1;
+            if(x.pos==1){
+                vit += 1;
+            }
+        }else
+        {
+            percent_fuoripod += 1;
+        }
+    });
+    giocatore.percentuale_vit = vit/gioc;
+    giocatore.percentuale_pod = percent_pod/gioc;
+    giocatore.percentuale_fuoripod = percent_fuoripod/gioc;
+}
+export function mediaPunteggi(giocatori)
+ {
+    let p =0; 
+    let g = 0;
+    giocatori.gare.forEach(x=>{
+        p += x.punti;
+        g +=1;
+    })
+    giocatori.media_punteggi = p/g;
+ }
